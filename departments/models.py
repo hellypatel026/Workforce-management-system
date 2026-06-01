64f4a5774232616b17083ee9baa3c673cde2c2d7
hellypatel026 <helly0026@gmail.com>
2026-06-01 09:35:10 +0530

    Added department and employee

--- departments/models.py
+++ departments/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from departments.models import Department
+from django.conf import settings
 
-# Create your models here.
+
+class Employee(models.Model):
+
+    ROLE_CHOICES = (
+        ('hr', 'HR'),
+        ('manager', 'Manager'),
+        ('worker', 'Worker'),
+    )
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    employee_id = models.CharField(max_length=20, unique=True)
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES
+    )
+
+    full_name = models.CharField(max_length=100)
+
+    department = models.ForeignKey(
+        Department,
+        on_delete=models.CASCADE
+    )
+
+    designation = models.CharField(max_length=100)
+
+    joining_date = models.DateField()
+
+    phone = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f"{self.employee_id} - {self.full_name}"
