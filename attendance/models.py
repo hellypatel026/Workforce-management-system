6c832568472b69e7e57ab53913a6199490b2bcc0
hellypatel026 <helly0026@gmail.com>
2026-06-03 09:26:38 +0530

    Added attendance model

--- attendance/models.py
+++ attendance/models.py
@@ -1,3 +1,33 @@
+from employees.models import Employee
 from django.db import models
 
-# Create your models here.
+class Attendance(models.Model):
+
+    employee = models.ForeignKey(
+        Employee,
+        on_delete=models.CASCADE
+    )
+
+    date = models.DateField()
+
+    check_in = models.TimeField(
+        null=True,
+        blank=True
+    )
+
+    check_out = models.TimeField(
+        null=True,
+        blank=True
+    )
+
+    status = models.CharField(
+        max_length=10,
+        choices=[
+            ('Present', 'Present'),
+            ('Absent', 'Absent'),
+        ],
+        default='Present'
+    )
+
+    def __str__(self):
+        return f"{self.employee.full_name} - {self.date}"
